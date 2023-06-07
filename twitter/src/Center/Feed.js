6415eb67dd8d96a5261f4e2b873c49dde2674206
fasiiha <fasihaa.arshad@gmail.com>
2023-06-08 01:15:25 +0500

    twitter

--- twitter/src/Center/Feed.js
+++ twitter/src/Center/Feed.js
@@ -1,49 +1,59 @@
-import {React, useState} from 'react'
-import './Feed.css'
-import Header from '../Header/Header'
-import HeaderOption from '../Header/HeaderOption'
+import React, { useState, useEffect } from 'react';
+import './Feed.css';
+import Header from '../Header/Header';
+import HeaderOption from '../Header/HeaderOption';
 import TweetBox from './TweetBox/TweetBox';
 import Tweet from './Tweet/Tweet';
-
 import FeaturedPlayListIcon from '@mui/icons-material/FeaturedPlayList';
 import PeopleIcon from '@mui/icons-material/People';
 import Diversity1Icon from '@mui/icons-material/Diversity1';
 
-export default function Feed({userName, filter}) {
+export default function Feed({ filter }) {
   const [activeOption, setActiveOption] = useState('For You');
+  const [tweets, setTweets] = useState([]);
 
   const handleOptionClick = (text) => {
     setActiveOption(text);
   };
 
+  
   let option1 = <HeaderOption text="For You" Icon={FeaturedPlayListIcon} />;
   let option2 = <HeaderOption text="Following" Icon={PeopleIcon} />;
   let option3 = <HeaderOption text="Circle" Icon={Diversity1Icon} />;
 
+  useEffect(() => {
+    
+    fetchTweetsFromDatabase()
+      .then((data) => {
+        setTweets(data);
+      })
+      .catch((error) => {
+        console.log('Error fetching tweets:', error);
+      });
+  }, []);
+
+const fetchTweetsFromDatabase = async () => {
+  try {
+    const response = await fetch('http://localhost:8080/demo');
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    throw new Error('Failed to fetch tweets from the database.');
+  }
+};
+
+
   return (
     <div className="feed">
-        {/* Header */}
-        {
-          userName == "" &&
-          <Header text={filter}/>
-        }
-        {
-          userName != "" &&
-          <Header text={userName} options={[option1, option2, option3]} handleOptionClick={handleOptionClick}/>
-        }
+      {/* Header */}
+      <Header text={filter} options={[option1, option2, option3]} handleOptionClick={handleOptionClick} />
 
-        {/* TweetBox */}
-        {
-          userName != "" &&
-          <TweetBox />
-        }
+      {/* TweetBox */}
+      <TweetBox />
 
-        {/* Post */}
-        <Tweet text="Hello World!"/>
-        <Tweet text="@elonmusk who?"/>
-        <Tweet text="#MuskHub"/>
-        <Tweet text="کیا مجھے پیار ہے؟"/>
-
+      {/* {tweets.map((tweet) => (
+        <Tweet key={tweet.id} text={tweet.text} />
+      ))} */}
     </div>
-  )
+  );
 }
